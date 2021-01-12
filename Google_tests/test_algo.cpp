fd40b285610ac8ecea7ba43643ec69354b028174
Q0un <qqun.y@yandex.ru>
2021-01-12 18:05:32 +0300

    Added tests for prefix function

--- Google_tests/test_algo.cpp
+++ Google_tests/test_algo.cpp
@@ -8,3 +8,19 @@
 TEST(PrefixFunctionTestSuite, 1) {
     EXPECT_EQ(findFirstOccurence("abacaba", "cab"), 3);
 }
+
+TEST(PrefixFunctionTestSuite, 2) {
+    EXPECT_EQ(findFirstOccurence("ababababb", "baba"), 1);
+}
+
+TEST(PrefixFunctionTestSuite, 3) {
+    EXPECT_EQ(findFirstOccurence("abcdefgh", "fg"), 5);
+}
+
+TEST(PrefixFunctionTestSuite, 4) {
+    EXPECT_EQ(findFirstOccurence("baaabacbabc", "abc"), 8);
+}
+
+TEST(PrefixFunctionTestSuite, 5) {
+    EXPECT_EQ(findFirstOccurence("aaabaabaaaaa", "aaaa"), 7);
+}
