2e035a63c58c6c18376a73313614f5fa65e627ef
Q0un <qqun.y@yandex.ru>
2021-01-12 15:43:20 +0300

    Added input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include "rule.h"
+#include <vector>
+#include <string>
+
+std::vector<Rule> scheme;
+int n_rules;
+std::string start_str;
+
+void read() {
+    std::cout << "Write number of rules in scheme:" << std::endl;
+    std::cin >> n_rules;
+    std::cout << "Write scheme:" << std::endl;
+    scheme.resize(n_rules);
+    for (int i = 0; i < n_rules; i++) {
+        std::cin >> scheme[i];
+    }
+    std::cout << "Write starting word:" << std::endl;
+    std::cin >> start_str;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    read();
 }
