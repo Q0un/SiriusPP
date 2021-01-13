bdc0227598449aa70f3dc138967e001a5053a339
Q0un <qqun.y@yandex.ru>
2021-01-13 10:11:59 +0300

    Added tests to solution

--- Google_tests/test_solution.cpp
+++ Google_tests/test_solution.cpp
@@ -6,5 +6,30 @@
 #include "../natural_algorithm.h"
 
 TEST(SolutionTestSuite, 1) {
-    // TODO
+    Algorithm algo(3, {Rule("ab", "a", false),
+                       Rule("aa", "b", false),
+                       Rule("a", "c", false)});
+    std::string str = "abacaba";
+    EXPECT_EQ(algo.applyAlgo(str), "bcb");
+}
+
+TEST(SolutionTestSuite, 2) {
+    Algorithm algo(2, {Rule("aaaa", "aaaax", true),
+                       Rule("a", "aa", false)});
+    std::string str = "a";
+    EXPECT_EQ(algo.applyAlgo(str), "aaaax");
+}
+
+TEST(SolutionTestSuite, 3) {
+    Algorithm algo(2, {Rule("a", "b", false),
+                       Rule("c", "d", true)});
+    std::string str = "x";
+    EXPECT_EQ(algo.applyAlgo(str), "x");
+}
+
+TEST(SolutionTestSuite, 4) {
+    Algorithm algo(2, {Rule("ab", "c", true),
+                       Rule("a", "b", false)});
+    std::string str = "aa";
+    EXPECT_EQ(algo.applyAlgo(str), "bb");
 }
